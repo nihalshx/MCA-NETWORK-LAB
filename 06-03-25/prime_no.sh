b47fcf0b231938038aee9c68fde54c736509923a
Nihal N <112004307+nihalshx@users.noreply.github.com>
2025-03-07 00:23:02 -0500

    Update prime_no.sh

--- 06-03-25/prime_no.sh
+++ 06-03-25/prime_no.sh
@@ -1,18 +1,17 @@
-echo "Enter starting number :"
-read first
-echo "Enter last number:"
-read last
-
-for((i=first;i<=last;i++))
+for((i=50;i<100;i++))
 do
-	((a= $i % 2))
-	if (($a!=0))
-	then
-	
-		echo -n ",$i"
-	fi
-	
+        prime=1
+        for((j=2;j<=$((i/2));j++))
+        do
+                if ((i%j==0))
+                then
+                        prime=0
+                        break
+                fi
+        done
+        if ((prime==1))
+        then
+                echo "$i is prime"
+        fi
 done
 
-	
-	
